88644fe8459c8cc9db6bd98527ee7f0fd2cd6925
CDORON@il.ibm.com <cdoron@lnx-arrow.sl.cloud9.ibm.com>
2021-06-09 19:57:22 +0300

    fix throughput measurement
    Signed-off-by: CDORON@il.ibm.com <cdoron@lnx-arrow.sl.cloud9.ibm.com>

--- Java-FlightServer/src/main/java/ibm/com/example/MyFlightClient.java
+++ Java-FlightServer/src/main/java/ibm/com/example/MyFlightClient.java
@@ -42,6 +42,6 @@ public class MyFlightClient {
         long finish = System.currentTimeMillis();
 
         System.out.println("Time spent traversing dataset: " + (finish - start)/1000.0 + " seconds");
-        System.out.println("Throughput: " + (long)i * root.getRowCount() * 4 * 4 / ((finish - start) / 1000.0));
+        System.out.println("Throughput: " + ((long)i * root.getRowCount() * 4 * 4 / ((finish - start) / (1000.0))) / (1024*1024) + " MB/sec");
     }
 }
