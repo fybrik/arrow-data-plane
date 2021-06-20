153092b00cd764d9ae0153344e71e65d1dca2336
CDORON@il.ibm.com <cdoron@lnx-arrow.sl.cloud9.ibm.com>
2021-06-20 16:25:32 +0300

    the two servers run in the same container
    Signed-off-by: CDORON@il.ibm.com <cdoron@lnx-arrow.sl.cloud9.ibm.com>

---
+++ Java-FlightServer/docker/create_docker_images.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -f classes.tar.gz ]
+  then echo creating tgz file
+  cd ../build/classes/java/main/
+  tar cvfz ../../../../docker/classes.tar.gz ibm/
+  cd -
+fi
+
+if [ ! -f logging.xml ]
+  then cp ../logging.xml .
+fi
+
+if [ ! -f Java-FlightServer-1.0-SNAPSHOT-all.jar ]
+  then cp ../build/libs/Java-FlightServer-1.0-SNAPSHOT-all.jar .
+fi
+
+
+docker build --tag example_server:latest .
---
+++ Java-FlightServer/docker/run_servers.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /root
+
+java --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -Dlogback.configurationFile=file:logging.xml -cp "Java-FlightServer-1.0-SNAPSHOT-all.jar:." ibm.com.example.server.ExampleFlightServer example &
+java --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -Dlogback.configurationFile=file:logging.xml -cp "Java-FlightServer-1.0-SNAPSHOT-all.jar:." ibm.com.example.server.ExampleFlightServer relay
