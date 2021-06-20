b90bdbf08f9bd42e08ac49c41a26851d227267bf
Mohammad-nassar10 <mohammad.nassar@ibm.com>
2021-06-20 12:36:30 +0300

    Move to wasm-modules/allocator.
    Signed-off-by: Mohammad-nassar10 <mohammad.nassar@ibm.com>

---
+++ wasm-modules/allocator/src/lib.rs
@@ -0,0 +1,28 @@
+
+use std::os::raw::c_void;
+
+
+
+#[no_mangle]
+pub fn alloc(len: i64) -> *mut c_void {
+    // create a new mutable buffer with capacity `len`
+    let mut buf = Vec::with_capacity(len as usize);
+    // take a mutable pointer to the buffer
+    let ptr = buf.as_mut_ptr();
+    // take ownership of the memory block and
+    // ensure that its destructor is not
+    // called when the object goes out of scope
+    // at the end of the function
+    std::mem::forget(buf);
+    // return the pointer so the runtime
+    // can write data at this offset
+    ptr as *mut c_void
+}
+
+#[no_mangle]
+pub unsafe fn dealloc(ptr: i64, size: i64) {
+    let data = Vec::from_raw_parts(ptr as *mut u8, size as usize, size as usize);
+    std::mem::drop(data);
+}
+
+
