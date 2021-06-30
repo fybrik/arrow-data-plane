f5e687ea64788c6a2775da308e4654925638ae38
Mohammad-nassar10 <mohammad.nassar@ibm.com>
2021-06-30 16:15:21 +0300

    Arrange comments.
    Signed-off-by: Mohammad-nassar10 <mohammad.nassar@ibm.com>

--- Java-wasm-allocator/src/main/org/m4d/adp/allocator/WasmAllocationManager.java
+++ Java-wasm-allocator/src/main/org/m4d/adp/allocator/WasmAllocationManager.java
@@ -22,9 +22,9 @@ public final class WasmAllocationManager extends AllocationManager {
     private final long wasmMemoryAddress;
 
     /**
-      * A constructor of WasmAllocationManager that allocated a memory chunk with the requested size in a WASM module memory
-      * using allocation function of that module.
-      */
+     * A constructor of WasmAllocationManager that allocated a memory chunk with the requested size in a WASM module memory
+     * using allocation function of that module.
+     */
     WasmAllocationManager(long instancePtr, BufferAllocator accountingAllocator, long requestedSize) {
         super(accountingAllocator);
         WasmAllocationManager.instancePtr = instancePtr;
@@ -34,24 +34,24 @@ public final class WasmAllocationManager extends AllocationManager {
     }
 
     /**
-    * Release memory chunk that is managed by this WasmAllocationManager.
-    */
+     * Release memory chunk that is managed by this WasmAllocationManager.
+     */
     @Override
     protected void release0() {
         AllocatorInterface.wasmDealloc(instancePtr, allocatedAddress, allocatedSize);
     }
 
     /**
-    * Get the size of the memory chunk that is managed by this WasmAllocationManager.
-    */
+     * Get the size of the memory chunk that is managed by this WasmAllocationManager.
+     */
     @Override
     public long getSize() {
         return allocatedSize;
     }
 
     /**
-    * Get the address address pointing to the fist byte of the memory chunk that is managed by this WasmAllocationManager.
-    */
+     * Get the address address pointing to the fist byte of the memory chunk that is managed by this WasmAllocationManager.
+     */
     @Override
     protected long memoryAddress() {
         return allocatedAddress + wasmMemoryAddress;
