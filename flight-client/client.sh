7494ecc1155e6deac4532848ac551c3a3fd040ff
Doron Chen <cdoron@il.ibm.com>
2021-07-08 14:49:34 +0300

    update scripts (#33)
    update scripts to point to new jars created by latest gradle configuration
    
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- flight-client/client.sh
+++ flight-client/client.sh
@@ -5,4 +5,4 @@ if [ $# -ne 2 ]; then
    exit 1
 fi
 
-java --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -Dlogback.configurationFile=file:../resources/logging.xml -cp "../build/libs/Java-FlightServer-1.0-SNAPSHOT-all.jar" org.m4d.adp.flight_client.MyFlightClient $1 $2
+java --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -Dlogback.configurationFile=file:../resources/logging.xml -cp "./build/libs/flight-client-all.jar" org.m4d.adp.flight_client.MyFlightClient $1 $2
--- flight-server/relay.sh
+++ flight-server/relay.sh
@@ -6,4 +6,4 @@ if [ $# -ne 1 ]; then
 fi
 
 
-java --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -Dlogback.configurationFile=file:../resources/logging.xml -cp "../build/libs/Java-FlightServer-1.0-SNAPSHOT-all.jar" org.m4d.adp.flight_server.ExampleFlightServer relay $1
+java --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -Dlogback.configurationFile=file:../resources/logging.xml -cp "./build/libs/flight-server-all.jar" org.m4d.adp.flight_server.ExampleFlightServer relay $1
