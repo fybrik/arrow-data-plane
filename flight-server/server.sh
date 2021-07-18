44024c606a6e4ca1bc7d7bfaf355038d984f2923
Mohammad-nassar10 <79787844+Mohammad-nassar10@users.noreply.github.com>
2021-07-18 14:35:02 +0300

    Integrate Wasm allocator in the example flight server. (#34)
    * Integrate Wasm allocator in the example flight server.
    
    Signed-off-by: Mohammad-nassar10 <mohammad.nassar@ibm.com>

--- flight-server/server.sh
+++ flight-server/server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -Dlogback.configurationFile=file:../resources/logging.xml -cp "./build/libs/flight-server-all.jar" org.m4d.adp.flight_server.ExampleFlightServer example localhost 12233
+java --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -Dlogback.configurationFile=file:../resources/logging.xml -cp "./build/libs/flight-server-all.jar" org.m4d.adp.flight_server.ExampleFlightServer -s example -h localhost --port 12233
