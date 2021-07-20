5140ccea6ba6cae998c94cdb91aaa88ac7c57536
tomersolomon1 <tomer.solomon1@gmail.com>
2021-07-20 16:55:32 +0300

    exporting record-batch
    Signed-off-by: tomersolomon1 <tomer.solomon1@gmail.com>

--- record-batch-jni/Java/jni_lib/src/main/java/com/ibm/arrowconverter/App.java
+++ record-batch-jni/Java/jni_lib/src/main/java/com/ibm/arrowconverter/App.java
@@ -1,23 +1,19 @@
 package com.ibm.arrowconverter;
 
-import com.google.common.collect.ImmutableList;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
-import org.apache.arrow.util.AutoCloseables;
-import org.apache.arrow.util.Preconditions;
-import org.apache.arrow.vector.BigIntVector;
-import org.apache.arrow.vector.IntVector;
 import org.apache.arrow.vector.FieldVector;
+import org.apache.arrow.vector.IntVector;
 import org.apache.arrow.vector.VectorSchemaRoot;
-import org.apache.arrow.vector.types.Types.MinorType;
 import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.Schema;
-import org.apache.arrow.memory.ArrowBuf;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 class ArrowJNIAdapter {
     // Returns a pointer to a Rust CDataInterface object, based on the scheme provided
-    private static native void Convert2CdataInterface(VectorSchemaRoot VectorSchemaRoot);
+    private static native void Convert2CdataInterface(VectorSchemaRoot vsr_src, VectorSchemaRoot vsr_target);
 
     static {
         // This actually loads the shared object that we'll be creating.
@@ -26,9 +22,7 @@ class ArrowJNIAdapter {
         System.loadLibrary("mylib");
     }
 
-    public static VectorSchemaRoot createDummyVSR() {
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-
+    public static VectorSchemaRoot createDummyVSR(BufferAllocator allocator) {
         IntVector a = new IntVector("a", allocator);
         IntVector b = new IntVector("b", allocator);
         IntVector c = new IntVector("c", allocator);
@@ -52,17 +46,11 @@ class ArrowJNIAdapter {
     }
 
     public static void main(String[] args) {
-        VectorSchemaRoot vsr = createDummyVSR();
-        //System.out.println("row count: " + vsr.getRowCount());
-        List<FieldVector> vectors = vsr.getFieldVectors();
-        ArrowBuf[] buffers = vectors.get(0).getBuffers(false);
-        System.out.println("SIZE: " + buffers.length);
-
-        //ArrowBuf buf = vectors.get(0).getDataBuffer();
-        //System.out.println("data buf length: " + vectors.get(0).getValueCount());
-
-        Schema schema =  vsr.getSchema();
-        //System.out.println("schema in java: " + schema);
-        ArrowJNIAdapter.Convert2CdataInterface(vsr);
+        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
+        VectorSchemaRoot vsr_src = createDummyVSR(allocator);
+        Schema schema =  vsr_src.getSchema();
+        VectorSchemaRoot vsr_target = VectorSchemaRoot.create(schema, allocator);
+        ArrowJNIAdapter.Convert2CdataInterface(vsr_src, vsr_target);
+        System.out.println("vsr_target content: \n" + vsr_target.contentToTSVString());
     }
 }
--- record-batch-jni/Rust/jni_lib/src/exporter.rs
+++ record-batch-jni/Rust/jni_lib/src/exporter.rs
@@ -5,7 +5,7 @@ use jni::JNIEnv;
 // These objects are what you should use as arguments to your native
 // function. They carry extra lifetime information to prevent them escaping
 // this context and getting used after being GC'd.
-use jni::objects::{JClass, JString, JObject, JValue};
+use jni::objects::{JClass, JString, JObject, JValue, JList};
 
 // This is just a pointer. We'll be returning it from our function. We
 // can't return one of the objects with lifetime information because the
@@ -39,89 +39,58 @@ pub fn from_rust_schema_to_java_schema<'a>(env: JNIEnv, r_schema: Schema) -> job
         .unwrap().into_inner()
 }
 
-
-// Create a reference manager using the memory allocator
-pub fn create_reference_manager(env: JNIEnv, jmem_allocator: JObject) -> jobject {
-    let jv_buffer_alocator = env.call_method(jmem_allocator, "getBufferAllocator", "()Lorg/apache/arrow/memory/BufferAllocator;", &[])
+pub fn create_arrow_buf_from_rust_buf(env: JNIEnv, buf: &Buffer) -> jobject {
+    let len = buf.len() as i64;
+    // Address of the memory
+    let ptr = buf.as_ptr() as i64;
+    // Creating the reference manager that will be accounting this buffer
+    //let j_reference_manager = JObject::from(create_reference_manager(env, jmem_allocator));
+    let jo_no_op_rm = env.get_static_field("org/apache/arrow/memory/ReferenceManager", "NO_OP", "Lorg/apache/arrow/memory/ReferenceManager;")
+        .unwrap()
+        .l()
         .unwrap();
-    let args = [jv_buffer_alocator, JValue::Object(jmem_allocator)];
-    env.new_object("Lorg/apache/arrow/memory/BufferLedger", "(Lorg/apache/arrow/memory/BufferAllocator;Lorg/apache/arrow/memory/AllocationManager;)V", &args)
+    // Create the ArrowBuf
+    let mut args = [JValue::Object(JObject::from(jo_no_op_rm)), JValue::Object(JObject::null()), JValue::Long(len), JValue::Long(ptr)];
+    env.new_object("org/apache/arrow/memory/ArrowBuf", "(Lorg/apache/arrow/memory/ReferenceManager;Lorg/apache/arrow/memory/BufferManager;JJ)V", &args)
         .unwrap()
         .into_inner()
-
 }
 
 // Populate a java vector with buffers from rust
-pub fn populate_java_vector(env: JNIEnv, r_arr : Arc<dyn Array>, jmem_allocator: JObject, j_field_vector: JObject) {
-    let list_buffers = env.new_object("Ljava/util/ArrayList", "()V", &[]).unwrap();
-    let r_buffers = r_arr.data().buffers();
+pub fn populate_java_vector(env: JNIEnv, r_arr : Arc<dyn Array>, j_field_vector: JObject) {
+    let list_buffers = JList::from_env(&env, env.new_object("java/util/ArrayList", "(I)V", &[JValue::Int(1 as i32)]).unwrap()).unwrap();
+    let r_buffers = [&r_arr.data().null_buffer().unwrap(), &r_arr.data().buffers()[0]];
 
     // Create ArrowFieldNode
     let length = r_arr.len() as i64;
     let null_count = r_arr.null_count() as i64;
-    let j_arrow_field_node = env.new_object("Lorg/apache/arrow/vector/ipc/message/ArrowFieldNode", "(JJ)V",
+    let j_arrow_field_node = env.new_object("org/apache/arrow/vector/ipc/message/ArrowFieldNode", "(JJ)V",
                                             &[JValue::Long(length), JValue::Long(null_count)])
         .unwrap();
 
     for i in 0..r_buffers.len() {
-        let buf = &r_buffers[i];
-        let len = buf.len() as i64;
-        // Address of the memory
-        let ptr = buf.as_ptr() as i64;
-
-        // Creating the reference manager that will be accounting this buffer
-        let j_reference_manager = JObject::from(create_reference_manager(env, jmem_allocator));
-
-        let mut args = [JValue::Object(JObject::from(j_reference_manager)), JValue::Object(JObject::null()), JValue::Long(len), JValue::Long(ptr)];
-
-        // Create the ArrowBuf
-        let j_arrow_buf = env.new_object("Lorg/apache/arrow/memory/ArrowBuf", "(Lorg/apache/arrow/memory/ReferenceManager;Lorg/apache/arrow/memory/BufferManager;JJ)V", &args)
-            .unwrap();
-
-        // Increment by 1 the number of references to the buffer in the buffer manager
-        env.call_method(j_reference_manager, "retain", "()V", &[]);
+        let r_buf = r_buffers[i];
+        let j_arrow_buf = JObject::from(create_arrow_buf_from_rust_buf(env, r_buf));
 
         // Add the buffer to the list of buffers
-        env.call_method(list_buffers, "add", "(Lorg/apache/arrow/memory/ArrowBuf;)V", &[JValue::Object(j_arrow_buf)]);
+        list_buffers.add(j_arrow_buf);
     }
-    let args = [JValue::Object(j_arrow_field_node), JValue::Object(list_buffers)];
-
     // Load the buffers to the vector
-    env.call_method(j_field_vector, "loadFieldBuffers", "(org/apache/arrow/vector/ipc/message/ArrowFieldNode;Ljava/util/List;)V", &args);
-
+    let args = [JValue::Object(j_arrow_field_node), JValue::Object(list_buffers.into())];
+    env.call_method(j_field_vector, "loadFieldBuffers", "(Lorg/apache/arrow/vector/ipc/message/ArrowFieldNode;Ljava/util/List;)V", &args).unwrap();
 }
 
-pub fn load_record_batch_to_vsr(env: JNIEnv, vsr: JObject, record: RecordBatch, jmem_allocator: JObject) {
-    let j_list_vectors = env.call_method(vsr, "getFieldVectors", "()V",&[])
-        .unwrap()
-        .l()
-        .unwrap();
+pub fn load_record_batch_to_vsr(env: JNIEnv, vsr: JObject, record: &RecordBatch) {
     for i in 0..record.columns().len() {
         let column = record.columns()[i].clone();
-        let j_field_vector = env.call_method(j_list_vectors, "get",
+        let j_field_vector = env.call_method(vsr, "getVector",
                                              "(I)Lorg/apache/arrow/vector/FieldVector;",
                                              &[JValue::Int(i as i32)])
             .unwrap()
             .l()
             .unwrap();
-        populate_java_vector(env, column, jmem_allocator, j_field_vector);
+        populate_java_vector(env, column, j_field_vector);
     }
+    let row_count = record.num_rows();
+    env.call_method(vsr, "setRowCount", "(I)V", &[JValue::Int(row_count as i32)]);
 }
-
-
-// Get a rust arrow-array, and returns a java FieldVector
-pub fn from_rust_arr_to_java_vector<'a>(env: JNIEnv, r_arr : &Array) -> JObject<'a>{
-    JObject::null()
-}
-
-// Get a rust columns, and returns a java List of FieldVector
-pub fn from_rust_colums_to_java_vectors<'a>(env: JNIEnv, columns: Vec<Arc<Array>>) -> JObject<'a> {
-    JObject::null()
-}
-
-// Get a rust record-batch, and returns a java VectorSchemaRoot
-pub fn from_rust_record_batch_to_java_vsr<'a>(env: JNIEnv, record: RecordBatch) -> JObject<'a> {
-    JObject::null()
-}
-
-
