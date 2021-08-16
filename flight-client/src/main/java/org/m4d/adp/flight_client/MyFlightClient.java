e01f95cb2008b51d480830e098fcbb20046b8604
Doron Chen <cdoron@il.ibm.com>
2021-08-16 08:25:03 +0300

    client to write only last record_batch
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- flight-client/src/main/java/org/m4d/adp/flight_client/MyFlightClient.java
+++ flight-client/src/main/java/org/m4d/adp/flight_client/MyFlightClient.java
@@ -3,6 +3,9 @@ package org.m4d.adp.flight_client;
 import org.apache.arrow.flight.*;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.BigIntVector;
+import org.apache.arrow.vector.FieldVector;
+import org.apache.arrow.vector.VarCharVector;
 import org.apache.arrow.vector.VectorSchemaRoot;
 
 import java.nio.charset.StandardCharsets;
@@ -42,20 +45,39 @@ public class MyFlightClient {
         long start = System.currentTimeMillis();
         while (s.next()) {
             root = s.getRoot();
-            i++;
+            //i++;
             // System.out.println(i);
             // System.out.println("client");
-            System.out.println(root.getVector(0));
-            System.out.println(root.getVector(1));
-            System.out.println(root.getVector(2));
-            System.out.println(root.getVector(3));
-            System.out.println(root.getVector(4));
-            System.out.println(root.getVector(5));
-            System.out.println(root.getVector(6));
+            //System.out.println(root.getVector(0));
+            //System.out.println(root.getVector(1));
+            //System.out.println(root.getVector(2));
+            //System.out.println(root.getVector(3));
+            //System.out.println(root.getVector(4));
+            //System.out.println(root.getVector(5));
+            //System.out.println(root.getVector(6));
         }
         long finish = System.currentTimeMillis();
 
-        System.out.println("Time spent traversing dataset: " + (finish - start)/1000.0 + " seconds");
+        VarCharVector names = (VarCharVector) root.getVector(0);
+        BigIntVector ages = (BigIntVector) root.getVector(1);
+        BigIntVector building_numbers = (BigIntVector) root.getVector(2);
+        VarCharVector streets = (VarCharVector) root.getVector(3);
+        VarCharVector cities = (VarCharVector) root.getVector(4);
+        VarCharVector countries = (VarCharVector) root.getVector(5);
+        BigIntVector postcodes = (BigIntVector) root.getVector(6);
+
+        for (int j = 0; j < root.getRowCount(); j++) {
+            System.out.print(names.get(j) + ",");
+            System.out.print(ages.get(j) + ",");
+            System.out.print(building_numbers.get(j) + ",");
+            System.out.print(streets.get(j) + ",");
+            System.out.print(cities.get(j) + ",");
+            System.out.print(countries.get(j) + ",");
+            System.out.println(postcodes.get(j));
+        }
+
+
+        System.out.println("\nTime spent traversing dataset: " + (finish - start)/1000.0 + " seconds");
         double throughput = (double)i * root.getRowCount() * 4 * 4 / ((finish - start) / 1000.0);
         System.out.println("Throughput: " + String.format("%.2f", throughput / (1024*1024)) + "MB/sec");
         try {
